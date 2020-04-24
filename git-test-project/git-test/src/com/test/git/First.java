d3d3c57e088f80f9401df6e1dee8d9ab769ed4ea
zhou <1459510934@qq.com>
2020-04-24 16:17:32 +0800

    添加git-test测试文件

---
+++ git-test-project/git-test/src/com/test/git/First.java
@@ -0,0 +1,15 @@
+package com.test.git;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        int aa = 0;
+        if (aa == 0 ){
+            aa++;
+            System.out.println(aa);
+        }
+    }
+}
